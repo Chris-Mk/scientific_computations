64fd93cfdcf02c8f491e5127927a21838d0c45a0
Chris <chrismks23@gmail.com>
2022-01-12 23:48:29 +0100

    redefined phi1 and phi2 with the sum average and difference average of the eigenfrequencies

--- beating.py
+++ beating.py
@@ -1,12 +1,16 @@
 from matplotlib.pyplot import *
 from numpy import *
 
-t = linspace(0, 40, 1000)
-phi1 = 0.05 * (cos(3.13 * t) + cos(3.44 * t))
-phi2 = 0.05 * (cos(3.13 * t) - cos(3.44 * t))
+t = linspace(0, 100, 1000)
+# phi1 = 0.05 * (cos(3.13 * t) + cos(3.44 * t))
+# phi2 = 0.05 * (cos(3.13 * t) - cos(3.44 * t))
+
+phi1 = 0.05 * (cos(3.29 * t) + cos(0.16 * t))
+phi2 = 0.05 * (cos(3.29 * t) - cos(0.16 * t))
 
 grid()
 xlabel("t")
 ylabel("phi(t)")
 plot(t, phi1)
 plot(t, phi2)
+# plot(t, phi1 + phi2)
